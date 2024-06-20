f66d413556e9547519233dd8e37cf6f410f87d3f
EnriquePalermo <enriqueandrespalermoleites@gmail.com>
2024-06-20 18:03:49 -0300

    Solucionando Tests general todo ok

--- src/test/java/TEST_BACK/Test_GET.java
+++ src/test/java/TEST_BACK/Test_GET.java
@@ -3,13 +3,15 @@ package TEST_BACK;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.gherkin.model.Given;
+
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
+
 import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
 import reportes.ReportFactory;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 import static io.restassured.RestAssured.given;
 
@@ -17,75 +19,119 @@ public class Test_GET {
     static ExtentSparkReporter info = new ExtentSparkReporter("reportes/APIGET-Test.html");
     static ExtentReports extent;
 
+    private static int authToken;
+    private static String username = "1234";
+    private static String password = "1234";
+
+
     @BeforeAll
     public static void setup() {
+        System.out.println("<<< COMIENZAN LOS TEST DE BACK >>>");
         extent = ReportFactory.getInstance();
         extent.attachReporter(info);
+
+        // Hacer la URL del login con usuario y contraseña con variables
+        String loginUrl = "https://parabank.parasoft.com/parabank/services/bank/login/{username}/{password}";
+
+        // Hago el GET para el login
+        Response response = given()
+                .pathParams("username", username)
+                .pathParams("password", password)
+                .when()
+                .get(loginUrl);
+
+        // Verifico que la respuesta sea correcta (200)
+        assertEquals(200, response.getStatusCode(), "Error al autenticar. Codigo de estado: " + response.getStatusCode());
+
+        // Extraigo el "customerId"(token de la pagina) de la respuesta XML
+        int customerId = response.xmlPath().getInt("customer.id");
+
+        // Pongo el token que tengo en el "customerID" en el authToken para poder usarlo en todas las pruebas que necesite
+        authToken = customerId;
     }
 
     @AfterAll
     public static void teardown() {
         extent.flush();
+        System.out.println("<<< FINALIZAN LOS TEST DE BACK >>>");
     }
 
     @Test
     @Tag("GET")
-    public void GET_Test01() {
-        ExtentTest test = extent.createTest("Primer Test GET de la API ReqRes");
+    public void GET_RegistroURL() {
+        ExtentTest test = extent.createTest("Test de Registro GET de la API");
         test.log(Status.INFO, "Comienza el Test");
-        System.out.println("Iniciando Primer Test Get");
-        test.log(Status.INFO, "Iniciando Primer Test Get");
+        System.out.println("Iniciando Registro Test Get");
+        test.log(Status.INFO, "Iniciando Registro Test Get");
 
-        Response responseGet = RestAssured.get("https://reqres.in/api/users?page=1");
-        System.out.println(responseGet.getBody().asString());
+        Response responseGet = RestAssured.get(" https://parabank.parasoft.com/parabank/register.htm");
         System.out.println(responseGet.statusCode());
+        test.log(Status.INFO, String.valueOf(responseGet.statusCode()));
         System.out.println(responseGet.getHeader("Date"));
         System.out.println(responseGet.getTime());
+        if (responseGet.getStatusCode() == 200) {
+            test.log(Status.PASS, "Validacion de estado 200 correcto");
+        } else {
+            test.log(Status.FAIL, "Validacion de estado fallido");
+        }
+        System.out.println("Registro Test Get finalizado");
 
-        System.out.println("Primer Test Get finalizado");
-        test.log(Status.PASS, "Primer Test Get finalizado");
     }
 
     @Test
     @Tag("GET")
-    public void GET_Test02() {
-        ExtentTest test = extent.createTest("Segundo Test GET de la API ReqRes");
+    public void GET_ResumenCuenta() {
+        ExtentTest test = extent.createTest("Test Resumen Cuenta GET de la API");
         test.log(Status.INFO, "Comienza el Test");
-        System.out.println("Iniciando Segundo Test Get");
-        test.log(Status.INFO, "Iniciando Segundo Test Get");
+        System.out.println("Iniciando Resumen Cuenta Test Get");
+        test.log(Status.INFO, "Iniciando Resumen Cuenta Test Get");
 
-        Response responseGet = RestAssured.get("https://reqres.in/api/users?page=1");
+        // Utilizo el ENDPOINT del Swagger de la pagina. No el que me da la letra del examen, porque no funciona. Utilizando el token para que sea mas automatizado.
+        Response responseGet = RestAssured.get("https://parabank.parasoft.com/parabank/services/bank/customers/"+authToken+"/accounts");
 
-        int statusCode = responseGet.statusCode();
-        JsonPath body = responseGet.jsonPath();
-        String nombre_1 = body.getString("data[0].first_name");
-        String mail_1 = body.getString("data[0].email");
+        System.out.println(responseGet.statusCode());
+        test.log(Status.INFO, String.valueOf(responseGet.statusCode()));
+        System.out.println(responseGet.getHeader("Date"));
+        System.out.println(responseGet.getTime());
+        if (responseGet.getStatusCode() == 200) {
+            test.log(Status.PASS, "Validacion de estado 200 correcto");
+        } else {
+            test.log(Status.FAIL, "Validacion de estado fallido");
+        }
+        System.out.println("Resumen Cuenta Test Get finalizado");
 
-        Assertions.assertEquals(nombre_1, "George");
-        Assertions.assertEquals(mail_1, "george.bluth@reqres.in");
-
-        System.out.println("EL CÓDIGO DEL RESPONSE ES: " + statusCode);
-        System.out.println("EL NOMBRE DEL PRIMER USUARIO DE LA LISTA ES: " + nombre_1);
-        System.out.println("EL MAIL DEL USUARIO" + nombre_1 + " ES: " + mail_1);
-
-        System.out.println("Segundo Test Get finalizado");
-        test.log(Status.PASS, "Segundo Test Get finalizado");
     }
 
     @Test
     @Tag("GET")
-    public void GET_Test03() {
-        ExtentTest test = extent.createTest("Tercer Test GET de la API ReqRes");
+    public void GET_ActividadCuenta() {
+        ExtentTest test = extent.createTest("Test Actividad Cuenta GET de la API");
         test.log(Status.INFO, "Comienza el Test");
-        System.out.println("Iniciando Tercer Test Get");
-        test.log(Status.INFO, "Iniciando Tercer Test Get");
+        System.out.println("Iniciando Actividad Cuenta Test Get");
+        test.log(Status.INFO, "Iniciando Actividad Cuenta Test Get");
 
-        given()
-                .get("https://reqres.in/api/users?page=2")
-                .then().statusCode(200)
-                .log().body();
 
-        System.out.println("Tercer Test Get finalizado");
-        test.log(Status.PASS, "Tercer Test Get finalizado");
+        Response responseGet = given()
+                .auth().basic(username,password)
+                .get("https://parabank.parasoft.com/parabank/services_proxy/bank/accounts/13899/transactions/month/All/type/All");
+
+        System.out.println(responseGet.statusCode());
+        test.log(Status.INFO, String.valueOf(responseGet.statusCode()));
+        System.out.println(responseGet.getHeader("Date"));
+        System.out.println(responseGet.getTime());
+        if (responseGet.getStatusCode() == 200) {
+            test.log(Status.PASS, "Validacion de estado 200 correcto");
+        } else {
+            test.log(Status.FAIL, "Validacion de estado fallido");
+        }
+        System.out.println("Actividad Cuenta Test Get finalizado");
+
     }
+
+
+
+
+
+
+
 }
--- src/test/java/TEST_BACK/Test_POST.java
+++ src/test/java/TEST_BACK/Test_POST.java
@@ -5,6 +5,7 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.google.gson.JsonObject;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
@@ -12,19 +13,44 @@ import org.junit.jupiter.api.Test;
 import reportes.ReportFactory;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Test_POST {
     static ExtentSparkReporter info = new ExtentSparkReporter("reportes/APIPOST-Test.html");
     static ExtentReports extent;
     ExtentTest test;
 
+    private static int authToken;
+    private static String username = "1234";
+    private static String password = "1234";
+
+
     @BeforeAll
     public static void setup() {
         extent = ReportFactory.getInstance();
         extent.attachReporter(info);
+
+        // Hacer la URL del login con usuario y contraseña con variables
+        String loginUrl = "https://parabank.parasoft.com/parabank/services/bank/login/{username}/{password}";
+
+        // Hago el GET para el login
+        Response response = given()
+                .pathParams("username", username)
+                .pathParams("password", password)
+                .when()
+                .get(loginUrl);
+
+        // Verifico que la respuesta sea correcta (200)
+        assertEquals(200, response.getStatusCode(), "Error al autenticar. Codigo de estado: " + response.getStatusCode());
+
+        // Extraigo el "customerId"(token de la pagina) de la respuesta XML
+        int customerId = response.xmlPath().getInt("customer.id");
+
+        // Pongo el token que tengo en el "customerID" en el authToken para poder usarlo en todas las pruebas que necesite
+        authToken = customerId;
     }
+
 
     @AfterAll
     public static void teardown() {
@@ -33,56 +59,61 @@ public class Test_POST {
 
     @Test
     @Tag("POST")
-    public void POST_Test01() {
-        test = extent.createTest("Primer Test POST de la API ReqRes");
+    public void POST_NuevaCuenta() {
+        test = extent.createTest("Test Nueva Cuenta POST de la API");
         test.log(Status.INFO, "Comienza el Test");
 
-        System.out.println("Iniciando Primer Test Post");
-        test.log(Status.INFO, "Iniciando Primer Test Post");
+        System.out.println("Iniciando Nueva Cuenta Test Post");
+        test.log(Status.INFO, "Iniciando Nueva Cuenta Test Post");
 
         JsonObject request = new JsonObject();
-        request.addProperty("name", "PACE");
-        request.addProperty("job", "PROFESOR");
+        request.addProperty("username", username);
+        request.addProperty("password", password);
+
 
         given()
+                .auth().basic(username, password)
                 .contentType("application/json")
-                .body(request)
-                .when().post("https://reqres.in/api/users")
+                .body(request.toString())
+                .when().post("https://parabank.parasoft.com/parabank/services_proxy/bank/createAccount?customerId=" + authToken + "&newAccountType=1&fromAccountId=13899")
                 .then()
-                .statusCode(201)
+                .statusCode(200)
                 .log().status()
                 .log().body();
 
-        System.out.println("Primer Test Post finalizado");
-        test.log(Status.PASS, "Primer Test Post finalizado");
+        System.out.println("Test Nueva Cuenta Post finalizado");
+        test.log(Status.INFO, "Validacion de estado 200 correcto");
+        test.log(Status.PASS, "Test Nueva Cuenta Post finalizado");
     }
 
     @Test
     @Tag("POST")
-    public void POST_Test02() {
-        test = extent.createTest("Segundo Test POST de la API ReqRes");
+    public void POST_Transferencia() {
+        test = extent.createTest("Test Transferencia POST de la API");
         test.log(Status.INFO, "Comienza el Test");
 
-        System.out.println("Iniciando Segundo Test Post");
-        test.log(Status.INFO, "Iniciando Segundo Test Post");
+        System.out.println("Iniciando Transferencia Test Post");
+        test.log(Status.INFO, "Iniciando Transferencia Test Post");
 
         JsonObject request = new JsonObject();
-        request.addProperty("name", "SERGIO");
-        request.addProperty("job", "PROFESOR");
+        request.addProperty("username", username);
+        request.addProperty("password", password);
+
 
         given()
+                .auth().basic(username, password)
                 .contentType("application/json")
-                .body(request)
-                .when().post("https://reqres.in/api/users")
+                .body(request.toString())
+                .when().post("https://parabank.parasoft.com/parabank/services_proxy/bank/transfer?fromAccountId=13899&toAccountId=15453&amount=100")
                 .then()
-                .statusCode(201)
-                .body("name", equalTo("SERGIO"))
-                .body("job", equalTo("PROFESOR"))
-                .body("createdAt", containsString("2024-06-06"))
+                .statusCode(200)
                 .log().status()
                 .log().body();
 
-        System.out.println("Segundo Test Post finalizado");
-        test.log(Status.PASS, "Segundo Test Post finalizado");
+        System.out.println("Test Transferencia Post finalizado");
+        test.log(Status.INFO, "Validacion de estado 200 correcto");
+        test.log(Status.PASS, "Test Transferencia Post finalizado");
     }
-}
+
+
+    }
